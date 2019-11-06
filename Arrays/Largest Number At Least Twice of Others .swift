334463f0c7d0c4b9af11a44770f2498311c5bb5e
Jeffrey Almonte <jalmonte83@gmail.com>
2019-11-05 20:00:20 -0500

    added a file to arrays folder

---
+++ Arrays/Largest Number At Least Twice of Others .swift
@@ -0,0 +1,13 @@
+//
+//  Largest Number At Least Twice of Others .swift
+//  
+//
+//  Created by Jeffrey Almonte on 11/5/19.
+//
+
+import Foundation
+
+func dominantIndex(_ nums: [Int]) -> Int {
+    var maxIndex = 0
+    
+}
